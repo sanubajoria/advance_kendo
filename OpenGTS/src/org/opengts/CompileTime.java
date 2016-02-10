e5c84b9d7b0f6f8db5425d86fd87ce381a235c1e
unknown <sbajoria@INW00007313.informatica.com>
2016-02-10 15:02:55 +0530

    new changes related to json

--- OpenGTS/src/org/opengts/CompileTime.java
+++ OpenGTS/src/org/opengts/CompileTime.java
@@ -1,12 +1,12 @@
 package org.opengts;
 public class CompileTime
 {
-    // 2015/12/30 01:01:33 IST
+    // 2016/02/08 14:23:53 IST
     public static final String SERVICE_ACCOUNT_ID   = "opengts";
     public static final String SERVICE_ACCOUNT_NAME = "Open Source OpenGTS";
     public static final String SERVICE_ACCOUNT_HASH = "8E8AD5BD55CCBA73A679AC720DC08B8A";
     public static final String SERVICE_ACCOUNT_KEY  = "";
-    public static final long   COMPILE_TIMESTAMP    = 1451417493L;
-    public static final String COMPILE_DATETIME     = "2015/12/30 01:01:33 IST";
-    public static final long   COMPILE_SEED         = 0xF816A74E34448E03L;
+    public static final long   COMPILE_TIMESTAMP    = 1454921633L;
+    public static final String COMPILE_DATETIME     = "2016/02/08 14:23:53 IST";
+    public static final long   COMPILE_SEED         = 0x592FCB78269BDA2DL;
 }
--- OpenGTS/src/org/opengts/war/track/page/AccountLogin.java
+++ OpenGTS/src/org/opengts/war/track/page/AccountLogin.java
@@ -51,7 +51,7 @@ import org.opengts.util.*;
 import org.opengts.dbtools.*;
 import org.opengts.db.*;
 import org.opengts.db.tables.*;
-
+import org.opengts.war.report.model.Serializer;
 import org.opengts.war.tools.*;
 import org.opengts.war.track.*;
 
@@ -262,40 +262,77 @@ public class AccountLogin
                 //      <input type="submit" name="submit" value="Login">
                 //  </form>
                 String cssLoginContent = borderedCss? CSS_LOGIN_CONTENT_CENTER : CSS_LOGIN_CONTENT_LEFT;
+                HashMap<String , Object > base = new HashMap<String ,Object>();
+                base.put("cssLoginContent", cssLoginContent);
+                
                 out.println("<table class='"+cssLoginContent+"' width='100%' cellpadding='0' cellspacing='0' border='0'>");
                 out.println("<tr>");
                 
                 String vsepImg = legacy? null : AccountLogin.this.getProperties().getString(PROP_VSeparatorImage,null);
                 if (!StringTools.isBlank(vsepImg)) {
+                   HashMap<String , String> vsepMap= new HashMap<String , String>();
+                   
+                   base.put("vsepImg", vsepMap);
                     String W = AccountLogin.this.getProperties().getString(PROP_VSeparatorImage_W,null);
                     String H = AccountLogin.this.getProperties().getString(PROP_VSeparatorImage_H,null);
                     out.print("<td class='"+CSS_LOGIN_VSEP_CELL+"'>");
                     out.print("<img ");
-                    if (!StringTools.isBlank(W)) { out.print(" width='"+W+"'"); }
-                    if (!StringTools.isBlank(H)) { out.print(" height='"+H+"'"); }
+                    if (!StringTools.isBlank(W)) { out.print(" width='"+W+"'");  vsepMap.put("W", W) ; }
+                    if (!StringTools.isBlank(H)) { out.print(" height='"+H+"'"); vsepMap.put("H", H) ;}
+                    vsepMap.put("src", vsepImg);
                     out.print(" src='"+vsepImg+"'/>");
                     out.print("</td>");
                 }
 
                 String cssLoginText = borderedCss? CSS_LOGIN_TEXT_CELL_CENTER : CSS_LOGIN_TEXT_CELL_LEFT;
+                base.put("cssLoginText", cssLoginText);
                 out.println("<td class='"+cssLoginText+"'>");
                 String enterLoginText = showPasswd?
                     i18n.getString("AccountLogin.enterLogin","Enter your Login ID and Password") :
                     i18n.getString("AccountLogin.enterLoginNoPass","Enter Login ID (No Password Required)");
+                base.put("enterLoginText", enterLoginText);
                 out.println("<span style='font-size:11pt'>"+enterLoginText+"</span>");
+                base.put("HR", HR);
                 out.println(HR);
+                HashMap<String,Object> form = new HashMap<String,Object>();
+                base.put("form", form);
                 //out.println("<center>"); // necessary because "text-align:center" doesn't center the following form/table
                 out.println("<form name='"+FORM_LOGIN+"' method='post' action='"+baseURL+"' target='"+target+"'>");
+                form.put("name", FORM_LOGIN);
+                form.put("action", baseURL);
+                form.put("target", target);
                 String cssLoginFormTable = borderedCss? CSS_LOGIN_FORM_PAD : CSS_LOGIN_FORM_NOPAD;
                 out.println("  <table class='"+cssLoginFormTable+"' cellpadding='0' cellspacing='0' border='0'>");
+                form.put("class", cssLoginFormTable);
+
                 String focusFieldID = "";
+                
+                ArrayList<Object> fields= new ArrayList<Object>();
+                HashMap<String, Object> field;
+
+                
                 // account login field
                 if (acctLogin) {
+                    field= new HashMap<String, Object>();
+                   field.put("label", i18n.getString("AccountLogin.account","Account:"));
+
                     String fldID = "accountLoginField";
                     out.print("  <tr><td class='accountLoginFieldLabel'>"+i18n.getString("AccountLogin.account","Account:")+"</td>");
                     out.print(      "<td class='accountLoginFieldValue'><input id='"+fldID+"' class='"+CommonServlet.CSS_TEXT_INPUT+"' type='text' "+ro+" name='"+Constants.PARM_ACCOUNT+"' value='"+accountID+"' size='24' maxlength='32'></td></tr>");
                     out.print("\n");
+                    field.put("id", fldID);
+                    field.put("class", CommonServlet.CSS_TEXT_INPUT);
+                    field.put("type", "text");
+                    field.put("ro", ro);
+                    field.put("name", Constants.PARM_ACCOUNT);
+                    field.put("value", accountID);
+                    field.put("size", "24");
+                    field.put("maxlength", "32");
+                    fields.add(field);
+
+
                     focusFieldID = fldID;
+                    form.put("focusFieldID", focusFieldID);
                 }
                 // user login field
                 if (userLogin && emailLogin) {
@@ -303,21 +340,60 @@ public class AccountLogin
                     out.print("  <tr><td class='accountLoginFieldLabel'>"+i18n.getString("AccountLogin.userEmail","User/EMail:")+"</td>");
                     out.print(      "<td class='accountLoginFieldValue'><input id='"+fldID+"' class='"+CommonServlet.CSS_TEXT_INPUT+"' type='text' "+ro+" name='"+Constants.PARM_USER+"' value='"+userID+"' size='30' maxlength='40'></td></tr>");
                     out.print("\n");
-                    if (StringTools.isBlank(focusFieldID)) { focusFieldID = fldID; }
+                    field= new HashMap<String, Object>();
+                    field.put("label", i18n.getString("AccountLogin.userEmail","User/EMail:"));
+
+                    field.put("id", fldID);
+                    field.put("class", CommonServlet.CSS_TEXT_INPUT);
+                    field.put("type", "text");
+                    field.put("ro", ro);
+                    field.put("name", Constants.PARM_USER);
+                    field.put("value", userID);
+                    field.put("size", "30");
+                    field.put("maxlength", "40");
+                    fields.add(field);
+
+                    if (StringTools.isBlank(focusFieldID)) { focusFieldID = fldID;                     form.put("focusFieldID", focusFieldID); }
                 } else
                 if (userLogin) {
                     String fldID = "userLoginField";
                     out.print("  <tr><td class='accountLoginFieldLabel'>"+i18n.getString("AccountLogin.user","User:")+"</td>");
                     out.print(      "<td class='accountLoginFieldValue'><input id='"+fldID+"' class='"+CommonServlet.CSS_TEXT_INPUT+"' type='text' "+ro+" name='"+Constants.PARM_USER+"' value='"+userID+"' size='24' maxlength='32'></td></tr>");
                     out.print("\n");
-                    if (StringTools.isBlank(focusFieldID)) { focusFieldID = fldID; }
+                    field= new HashMap<String, Object>();
+                    field.put("label", i18n.getString("AccountLogin.user","User:"));
+
+                    field.put("id", fldID);
+                    field.put("class", CommonServlet.CSS_TEXT_INPUT);
+                    field.put("type", "text");
+                    field.put("ro", ro);
+                    field.put("name", Constants.PARM_USER);
+                    field.put("value", userID);
+                    field.put("size", "24");
+                    field.put("maxlength", "32");
+                    fields.add(field);
+
+                    if (StringTools.isBlank(focusFieldID)) { focusFieldID = fldID;                     form.put("focusFieldID", focusFieldID); }
                 } else
                 if (emailLogin) {
                     String fldID = "emailLoginField";
                     out.print("  <tr><td class='accountLoginFieldLabel'>"+i18n.getString("AccountLogin.email","EMail:")+"</td>");
                     out.print(      "<td class='accountLoginFieldValue'><input id='"+fldID+"' class='"+CommonServlet.CSS_TEXT_INPUT+"' type='text' "+ro+" name='"+Constants.PARM_USEREMAIL+"' value='"+userID+"' size='30' maxlength='40'></td></tr>");
                     out.print("\n");
-                    if (StringTools.isBlank(focusFieldID)) { focusFieldID = fldID; }
+                    field= new HashMap<String, Object>();
+                    field.put("label", i18n.getString("AccountLogin.email","EMail:"));
+
+                    field.put("id", fldID);
+                    field.put("class", CommonServlet.CSS_TEXT_INPUT);
+                    field.put("type", "text");
+                    field.put("ro", ro);
+                    field.put("name", Constants.PARM_USEREMAIL);
+                    field.put("value", userID);
+                    field.put("size", "30");
+                    field.put("maxlength", "40");
+                    fields.add(field);
+
+                    if (StringTools.isBlank(focusFieldID)) { focusFieldID = fldID;                     form.put("focusFieldID", focusFieldID); }
                 }
                 // password field
                 if (showPasswd) {
@@ -325,9 +401,22 @@ public class AccountLogin
                     out.print(    "<td class='accountLoginFieldLabel'>"+i18n.getString("AccountLogin.password","Password:")+"</td>");
                     out.print(    "<td class='accountLoginFieldValue'><input class='"+CommonServlet.CSS_TEXT_INPUT+"' type='password' "+ro+" name='"+Constants.PARM_PASSWORD+"' value='' size='24' maxlength='32'></td>");
                     out.print(  "</tr>\n");
+                    field= new HashMap<String, Object>();
+                    field.put("label",i18n.getString("AccountLogin.password","Password:"));
+
+                    field.put("id", "");
+                    field.put("class", CommonServlet.CSS_TEXT_INPUT);
+                    field.put("type", "password");
+                    field.put("ro", ro);
+                    field.put("name", Constants.PARM_PASSWORD);
+                    field.put("value", "");
+                    field.put("size", "24");
+                    field.put("maxlength", "32");
+                    fields.add(field);
+
                 }
                 // language selection
-                if (showLocale) {
+                if (true) {
                     String dftLocale = privLabel.getLocaleString();
                     Map<String,String> localeMap = BasicPrivateLabel.GetSupportedLocaleMap(privLabel.getLocale());
                     ComboMap comboLocaleMap = new ComboMap(localeMap);
@@ -337,19 +426,45 @@ public class AccountLogin
                     out.write(      Form_ComboBox(CommonServlet.PARM_LOCALE, CommonServlet.PARM_LOCALE, true, comboLocaleMap, dftLocale, null/*onchange*/));
                     out.print(    "</td>");
                     out.print(  "</tr>\n");
+                    field= new HashMap<String , Object>();
+                    field.put("label",i18n.getString("AccountLogin.language","Language:"));
+
+                    field.put("id", CommonServlet.PARM_LOCALE);
+                    field.put("class", CommonServlet.CSS_ADMIN_COMBO_BOX);
+                    field.put("type", "combobox");
+                    field.put("ro", ro);
+                    field.put("option", localeMap);
+                    fields.add(field);
+
+      
+                    
                 }
                 // end table
                 out.print("</table>\n");
                 // Login
                 out.print("<br>");
                 out.print("<input type='submit' name='submit' value='"+i18n.getString("AccountLogin.login","Login")+"'>\n");
-                // forgot password
+                field= new HashMap<String, Object>();
+           //     field.put("label",i18n.getString("AccountLogin.language","Language:"));
+                
+            //    field.put("id", CommonServlet.PARM_LOCALE);
+                field.put("class", CommonServlet.CSS_ADMIN_COMBO_BOX);
+                field.put("type", "submit");
+                field.put("name", "submit");
+                field.put("value", i18n.getString("AccountLogin.login","Login"));
+
+                fields.add(field);                // forgot password
                 if (showPasswd && (forgotURL != null)) {
                    if (legacy) { out.println("<br>"); }
                    out.println("  <span style='font-size:8pt;padding-left:10px;'><i><a href='"+forgotURL+"'>"+i18n.getString("AccountLogin.forgotPassword","Forgot your password?")+"</a></i></span>");
                 }
+                
+                form.put("fields", fields);
+
                 // end forn
                 out.println("</form>");
+                base.put("form", form);
+             out.println( "fnale json"+  new Serializer().json(base));
                 // "Cookies/JavaScript must be enabled"
                 out.println("<br/>");
                 out.println("<span style='font-size:8pt'><i>"+i18n.getString("AccountLogin.cookiesJavaScript","(Cookies and JavaScript must be enabled)")+"</i></span>");
@@ -379,6 +494,7 @@ public class AccountLogin
                 
                 /* set focus */
                 if (!StringTools.isBlank(focusFieldID)) {
+                   
                     out.write("<script type=\"text/javascript\">\n");
                     out.write("var loginFocusField = document.getElementById('"+focusFieldID+"');\n");
                     out.write("if (loginFocusField) {\n");
